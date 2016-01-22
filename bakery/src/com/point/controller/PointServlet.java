0e84c877e6308b2d93e1d6b1ba25bf4316f8f188
king <king@gmail.cim>
2016-01-22 21:12:06 +0800

    98

--- bakery/src/com/point/controller/PointServlet.java
+++ bakery/src/com/point/controller/PointServlet.java
@@ -43,12 +43,21 @@ public class PointServlet extends HttpServlet {
 		MemberBean bean = (MemberBean) session.getAttribute("isLogin");// 抓他的登入
 		int mbId = bean.getMember_id();
 		MemberBean po = sr.selectp100(mbId); // 用我程式去修改他的點數id
+		
+//		session.setAttribute("point", po.getPoint());
+//		if(po.getPoint()<=0){
+//			String path = req.getContextPath();
+//			resp.sendRedirect(path+"/index.jsp");		
+//		}
+//		else {
+			
 		session.setAttribute("isLogin", po);
 		po.setMpicture(Base64.encodeBase64String(po.getPicture()));
 		
 		JSONObject jsonObjectMary = new JSONObject(po);	
 		out.print(jsonObjectMary);
 		}
+	
 		// System.out.println(poit.getPoint());
 //		req.setAttribute("upd", po);
 //		req.getRequestDispatcher("/front/activity/Jiugongge_OK.jsp").forward(req, resp);
