cb7a052ca796e33ab2f57d8a9e916f3f0cbeac52
黃豊傑 <a0938988920@gmail.com>
2016-01-25 09:37:38 +0800

    d

---
+++ bakery/src/com/friend/model/FriendBean.java
@@ -0,0 +1,86 @@
+package com.friend.model;
+
+public class FriendBean {
+	private Integer invite_id;//我
+	private Integer invitee_id;//哪些好友
+	private Integer friendstatu; //0.邀請  1.朋友 2.黑名單
+	
+	private String inviteAccount;//join member
+	private String inviteeAccount;//join member
+	private byte[] invitePiture;
+	private byte[] inviteePicture;
+	private String MinvitePiture; //**用來裝照片 轉型用
+	private String MinviteePicture;//**用來裝照片 轉型用
+	private String inviteeKaban;//**我的好友的介紹
+	private String inviteeNickname;//**我的好友的暱稱
+
+	public String getInviteeKaban() {
+		return inviteeKaban;
+	}
+	public void setInviteeKaban(String inviteeKaban) {
+		this.inviteeKaban = inviteeKaban;
+	}
+	public String getInviteeNickname() {
+		return inviteeNickname;
+	}
+	public void setInviteeNickname(String inviteeNickname) {
+		this.inviteeNickname = inviteeNickname;
+	}
+	public byte[] getInvitePiture() {
+		return invitePiture;
+	}
+	public void setInvitePiture(byte[] invitePiture) {
+		this.invitePiture = invitePiture;
+	}
+	public byte[] getInviteePicture() {
+		return inviteePicture;
+	}
+	public void setInviteePicture(byte[] inviteePicture) {
+		this.inviteePicture = inviteePicture;
+	}
+	public String getMinvitePiture() {
+		return MinvitePiture;
+	}
+	public void setMinvitePiture(String minvitePiture) {
+		MinvitePiture = minvitePiture;
+	}
+	public String getMinviteePicture() {
+		return MinviteePicture;
+	}
+	public void setMinviteePicture(String minviteePicture) {
+		MinviteePicture = minviteePicture;
+	}
+	public String getInviteAccount() {
+		return inviteAccount;
+	}
+	public void setInviteAccount(String inviteAccount) {
+		this.inviteAccount = inviteAccount;
+	}
+	public String getInviteeAccount() {
+		return inviteeAccount;
+	}
+	public void setInviteeAccount(String inviteeAccount) {
+		this.inviteeAccount = inviteeAccount;
+	}
+	public Integer getInvite_id() {
+		return invite_id;
+	}
+	public void setInvite_id(Integer invite_id) {
+		this.invite_id = invite_id;
+	}
+	public Integer getInvitee_id() {
+		return invitee_id;
+	}
+	public void setInvitee_id(Integer invitee_id) {
+		this.invitee_id = invitee_id;
+	}
+	public Integer getFriendstatu() {
+		return friendstatu;
+	}
+	public void setFriendstatu(Integer friendstatu) {
+		this.friendstatu = friendstatu;
+	}
+	
+	
+	
+}
---
+++ bakery/src/com/message/model/MessageService.java
@@ -0,0 +1,92 @@
+package com.message.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class MessageService {
+
+	public MessageDAO dao;
+
+	public MessageService() {
+		dao = new MessageDAOJndi();
+	}
+
+	public MessageBean selectByTime(Integer send_id, Integer read_id, Timestamp msg_date) {
+		return dao.select(send_id, read_id, msg_date);
+	}
+	
+	public MessageBean selectMessage(Integer msg_id , Integer member_id) {
+		return dao.selectMessage(msg_id , member_id);
+	}
+
+
+	public int insertmessage(MessageBean bean) {
+		return dao.insert(bean);
+	}
+
+	public int updatemessage(MessageBean bean) {
+		return dao.update(bean);
+	}
+	
+	public int updateState(MessageBean bean) {
+		return dao.updateState(bean);
+	}
+	
+
+	// 依照reader statu 來分頁數
+	public List<MessageBean> seletPage(int pageInt, Integer read_id,
+			Integer msg_state) {
+
+		return dao.selectPage(pageInt, read_id, msg_state);
+	}
+
+	// 依照reader 來分頁數
+	public List<MessageBean> seletAllPage(int pageInt, Integer read_id) {
+		return dao.selectAllPage(pageInt, read_id);
+	}
+
+	public List<MessageBean> selectAllByStateAndId(Integer read_id,
+			Integer msg_state) {
+		return dao.getgivemymsg(read_id, msg_state);
+	}
+
+	public int getMemberCount() {
+		return dao.getProduct();
+	}
+	
+	//**拿來抓新訊息用
+	public int getState(Integer msg_state){
+		return dao.getState(msg_state);
+	}
+
+	///**前台用
+	public int givemecount(Integer read_id,Integer msg_state){
+		return dao.givemecount(read_id, msg_state);
+	}
+	
+	public int getReadCount(Integer read_id) {
+		return dao.getreadCount(read_id);
+	}
+
+	public List<MessageBean> selectMessage() {
+		return dao.selectAll();
+	}
+
+	public MessageBean seletStatus() {
+		return dao.select();
+	}
+
+	
+	public void delete(Integer msg_id){
+		if(msg_id!=0){
+			dao.delete(msg_id);
+		}
+	}
+	
+	public MessageBean seletemsgid(Integer msg_id){	
+			MessageBean memberbean =dao.seletemsgid(msg_id);
+			return memberbean;	
+	}
+	
+	
+}
